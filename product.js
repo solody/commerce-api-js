a33fbe1967657b07034546bb13893651797909d2
kentkent <kent@solody.com>
2018-04-28 16:04:51 +0800

    fix

--- product.js
+++ product.js
@@ -3,9 +3,12 @@ export default {
     return http.get('api/rest/views/products/' + id + '?_format=json')
   },
   getProductVariations (http, id) {
-    return http.get('api/rest/views/product-variations/' + id)
+    return http.get('api/rest/views/product-variations/' + id + '?_format=json')
   },
   getBookingUnits (http, id) {
-    return http.get('api/rest/views/booking-units/' + id)
+    return http.get('api/rest/views/booking-units/' + id + '?_format=json')
+  },
+  searchProduct (http, keywords, type, page) {
+    return http.get('api/rest/views/product-search?_format=json&search_api_fulltext=' + keywords + '&page=' + page + '&type=' + type)
   }
 }
