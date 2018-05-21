fb55ad9a555b19cfdbd300a683d96f323856b3bf
solody <12943125@qq.com>
2018-05-21 12:40:42 +0800

    add api

--- booking.js
+++ booking.js
@@ -4,5 +4,11 @@ export default {
   },
   createBooking (http, data) {
     return http.post('api/rest/order-verification/code-appointment?_format=json', data)
+  },
+  getBookings (http, phone, code, page) {
+    let reqUrl = 'api/rest/views/aiqilv-order-verification/code-appointments?_format=json&page=' + page
+    if (phone) reqUrl += '&info_phone=' + phone
+    if (code) reqUrl += '&code=' + code
+    return http.get(reqUrl)
   }
 }
