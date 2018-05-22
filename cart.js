9ee8150c97430a76ee098495b82cbb379c6e6b09
solody <12943125@qq.com>
2018-05-22 13:47:59 +0800

    add api

--- cart.js
+++ cart.js
@@ -1,6 +1,12 @@
 export default {
+  getCarts (http) {
+    return http.get('cart?_format=json')
+  },
   add (http, data) {
     return http.post('cart/add?_format=json', data)
+  },
+  clear (http, cart) {
+    return http.delete('cart/' + cart + '/items?_format=json')
   },
   getCompleteOrder (http, id) {
     return http.get('api/rest/checkout/complete-order/' + id + '?_format=json')
