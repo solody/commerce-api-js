81015ace372f0b648db0e09f136c015068570845
solody <12943125@qq.com>
2018-05-29 16:09:19 +0800

    add api

--- order.js
+++ order.js
@@ -3,5 +3,10 @@ export default {
     let reqUrl = 'api/rest/views/orders/' + userId + '?_format=json&page=' + page
     if (phone) reqUrl += '&field_phone_value=' + phone
     return http.get(reqUrl)
+  },
+  getListByStatus (http, userId, page, state) {
+    let reqUrl = 'api/rest/views/orders/' + userId + '?_format=json&page=' + page
+    if (state && state !== 'all') reqUrl += '&state=' + state
+    return http.get(reqUrl)
   }
 }
