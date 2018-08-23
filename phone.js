007f8aa18b922227a91ed60ac94b3e399cd431e1
solody <12943125@qq.com>
2018-08-23 19:34:41 +0800

    fix login

--- phone.js
+++ phone.js
@@ -2,11 +2,12 @@ export default {
   SmsVerify (http, phone) {
     return http.post('api/rest/phone-verify/sms-code-verify?_format=json', {phone})
   },
-  smsLogin (http, clientId, phone, smsCode) {
+  smsLogin (http, clientId, phone, smsCode, autoRegister) {
     return http.post('api/rest/phone-login/phone-sms-login?_format=json', {
       client_id: clientId,
       phone,
-      code: smsCode
+      code: smsCode,
+      auto_register: autoRegister
     })
   }
 }
