908d1c40b0a7b3fa2d7c8b5185ed276d881bdda4
solody <12943125@qq.com>
2018-09-17 22:52:37 +0800

    fix

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 export default {
   getUserInfo (http) {
-    return http.get('api/rest/user-api/user-info?_format=json')
+    return http.get('api/rest/user-api/user-info?_format=json&time=' + Math.round(new Date().getTime() / 1000))
   },
   uploadAvatar (http, userId, data) {
     return http.post('api/rest/user-api/upload-avatar/' + userId + '?_format=json', {
