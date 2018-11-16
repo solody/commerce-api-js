d95d1b4eda9f44423516d01ddc7cfd806607a08a
kentkent <kent@solody.com>
2018-11-16 20:27:32 +0800

    fix

--- express.js
+++ express.js
@@ -1,6 +1,6 @@
 export default {
   getExpressMethods (http) {
-    return http.get('api/rest/commerce-express/express-method-list?_format=json')
+    return http.get('api/rest/commerce-express/express-method-list?_format=json&time=' + Math.round(new Date().getTime() / 1000))
   },
   setOrderExpress (http, orderId, expressMethod) {
     return http.post('api/rest/commerce-express/set-order-express?_format=json', {
